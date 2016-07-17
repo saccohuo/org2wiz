0f497aa9bcc56f5e8aa1544e40b18373cb00da09
husky <shuaike945@gmail.com>
2016-07-17 11:21:23 +0800

    modify filenames and add option for MJ and Default Tag

---
+++ DefaultTag.js
@@ -0,0 +1,12 @@
+﻿var objApp = WizExplorerApp;
+var objWindow = objApp.Window;
+var org_mode_pluginPath = objApp.GetPluginPathByScriptFileName("Org2Wiz.js");
+var objCommon = objApp.CreateActiveXObject("WizKMControls.WizCommonUI");
+var omOptionFileName = org_mode_pluginPath + "options.ini";
+var omDefaultTag = objCommon.GetValueFromIni(omOptionFileName, "Options", "DefaultTag");
+// objWindow.ShowMessage(omOptionFileName.toString(), "omOptionFileName",0);
+
+var newomDefaultTag = objCommon.InputBox("默认标签", "添加 Org2Wiz 默认添加的标签，例如 “原创”。对大小写敏感。", omDefaultTag);
+if(newomDefaultTag != ""){
+  objCommon.SetValueToIni(omOptionFileName, "Options", "DefaultTag", newomDefaultTag);
+}
---
+++ MathJaxOption.js
@@ -0,0 +1,12 @@
+﻿var objApp = WizExplorerApp;
+var objWindow = objApp.Window;
+var org_mode_pluginPath = objApp.GetPluginPathByScriptFileName("Org2Wiz.js");
+var objCommon = objApp.CreateActiveXObject("WizKMControls.WizCommonUI");
+var omOptionFileName = org_mode_pluginPath + "options.ini";
+var omMJOption = objCommon.GetValueFromIni(omOptionFileName, "Options", "MathJaxOption");
+// objWindow.ShowMessage(omOptionFileName.toString(), "omOptionFileName",0);
+
+var newomMJOption = objCommon.InputBox("MathJax 设置", "No 不使用 MathJax；Online 使用在线 MathJax；Offline 使用本地 MathJax。对大小写不敏感。", omMJOption);
+if(newomMJOption != ""){
+  objCommon.SetValueToIni(omOptionFileName, "Options", "MathJaxOption", newomMJOption);
+}
